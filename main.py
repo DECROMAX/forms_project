f090f94d00dad287ca043f1919c62aa455945dc8
DECROMAX <45635865+DECROMAX@users.noreply.github.com>
2021-05-29 22:52:23 +0100

    Added registration form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4456034fee525550a1ed73c1f675021c'
 
 posts = [
     {
@@ -26,6 +28,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
